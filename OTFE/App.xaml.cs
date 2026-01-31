89fe8cbb1b9a33cb509569e38baf0a45e0d0ef58
Tof <krzysztof.habich@outlook.com>
2026-01-31 13:09:02 -0500

    Add project files.

---
+++ OTFE/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace OTFE
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
